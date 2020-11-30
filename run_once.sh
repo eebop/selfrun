4f123708a49cb048342070935694fb04dade0914
Kieran <kieran.bieniosek@gmail.com>
2020-11-30 09:47:18 -0800

    add compatability

--- run_once.sh
+++ run_once.sh
@@ -2,6 +2,12 @@
 
 if [ -e ~/.bash_profile ]; then
   echo "export PATH=\"\$PATH:`pwd`\"" >> ~/.bash_profile
-else
+  echo "alias selfrun=\"selfrun.py\""
+elif [-e ~/.bashrc]; then
   echo "export PATH=\"\$PATH:`pwd`\"" >> ~/.bashrc
+  echo "alias selfrun=\"selfrun.py\""
+else
+  echo "please add these lines to your system's equivilent of ~/.bashrc or ~/.bash_profile:"
+  echo "export PATH=\"\$PATH:`pwd`\""\
+  echo "alias selfrun=\"selfrun.py\""
 fi
